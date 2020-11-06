d109f311b2fde869779441770164ae00b6797d3d
Nikolas Kolovos <nkolovos@uwo.ca>
2020-11-06 15:34:03 -0500

    adding game folder

---
+++ game/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.message}</h1>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+export default App;
